6a5fcb9cd51ceffec63aff51eba9f56c9afb7bac
[dyad] <rodeorollsagency@gmail.com>
2025-11-15 02:15:08 +0100

    [dyad] wrote 1 file(s)

--- src/pages/OfferPage.tsx
+++ src/pages/OfferPage.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } => 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
